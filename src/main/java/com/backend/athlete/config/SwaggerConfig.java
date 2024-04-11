58c240fe0c5e102b66b1a3f5c1dceccba754fe65
94-c <hyeongwoo26@gmail.com>
2024-04-11 14:15:17 +0900

    [feat] Swagger 적용 중

---
+++ src/main/java/com/backend/athlete/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.backend.athlete.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// SwaggerConfig.java
+@OpenAPIDefinition(
+        info = @Info(title = "채팅서비스 API 명세서",
+                description = "헥사고날 아키텍처 기반 채팅 서비스 API 명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("채팅서비스 API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
