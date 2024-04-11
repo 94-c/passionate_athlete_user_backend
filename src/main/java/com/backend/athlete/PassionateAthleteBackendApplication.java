ce09a4f83dd40972453b7453eec67f57292a27f0
94-c <hyeongwoo26@gmail.com>
2024-04-11 13:58:56 +0900

    first commit

---
+++ src/main/java/com/backend/athlete/PassionateAthleteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.athlete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassionateAthleteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassionateAthleteBackendApplication.class, args);
+    }
+
+}
