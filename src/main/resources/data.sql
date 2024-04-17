52891cbe108db3c34578b6dce87a65088d258566
94-c <hyeongwoo26@gmail.com>
2024-04-17 17:01:55 +0900

    [feat] Role 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles(role_name) VALUES ('ADMIN');
+INSERT INTO roles(role_name) VALUES ('MANAGER');
+INSERT INTO roles(role_name) VALUES ('USER');
+INSERT INTO roles(role_name) VALUES ('GUEST');
