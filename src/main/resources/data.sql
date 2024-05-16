08e0633f8be795725bce581a95a583a611a14917
94-c <hyeongwoo26@gmail.com>
2024-05-16 18:09:17 +0900

    remove : data.sql 삭제

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO roles(role_name) VALUES ('ADMIN');
-INSERT INTO roles(role_name) VALUES ('MANAGER');
-INSERT INTO roles(role_name) VALUES ('USER');
-INSERT INTO roles(role_name) VALUES ('GUEST');
