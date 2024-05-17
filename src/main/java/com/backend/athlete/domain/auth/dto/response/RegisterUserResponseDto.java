0ae49134045931cefbceeb86596c0c99721c4798
94-c <hyeongwoo26@gmail.com>
2024-05-17 14:38:26 +0900

    feat : 회원 정보 추가 및 권한 부여

--- src/main/java/com/backend/athlete/domain/auth/dto/response/RegisterUserResponseDto.java
+++ src/main/java/com/backend/athlete/domain/auth/dto/response/RegisterUserResponseDto.java
@@ -1,36 +1,48 @@
 package com.backend.athlete.domain.auth.dto.response;
 
 import com.backend.athlete.domain.user.model.User;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class RegisterUserResponseDto {
 
+    private String code;
     private String userId;
-    private String password;
     private String name;
-    private String roles;
-    @Builder
-    public RegisterUserResponseDto(String userId, String password, String name, String roles) {
+    private String gender;
+    private Double weight;
+    private Double height;
+    private Set<String> roles;
+
+    public RegisterUserResponseDto(String code, String userId, String name, String gender, Double weight, Double height, Set<String> roles) {
+        this.code = code;
         this.userId = userId;
-        this.password = password;
         this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
         this.roles = roles;
     }
 
     // Entity -> Dto
     public static RegisterUserResponseDto fromEntity(User user) {
-        return RegisterUserResponseDto.builder()
-                .userId(user.getUserId())
-                .name(user.getName())
-                .password(user.getPassword())
-                .roles(user.getRoles().name())
-                .build();
-    }
+        Set<String> roleNames = user.getRoles().stream()
+                .map(role -> role.getName().toString())
+                .collect(Collectors.toSet());
 
+        return new RegisterUserResponseDto(
+                user.getCode(),
+                user.getUserId(),
+                user.getName(),
+                user.getGender().toString(),
+                user.getWeight(),
+                user.getHeight(),
+                roleNames
+        );
+    }
 }
--- src/main/java/com/backend/athlete/domain/user/model/User.java
+++ src/main/java/com/backend/athlete/domain/user/model/User.java
@@ -1,75 +1,78 @@
 package com.backend.athlete.domain.user.model;
 
+import com.backend.athlete.domain.user.model.type.UserStatusType;
+import com.backend.athlete.domain.user.model.type.UserGenderType;
 import com.backend.athlete.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
-import lombok.Builder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Comment;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Entity
-@NoArgsConstructor
 @Table(name = "users")
-public class User extends BaseTimeEntity implements UserDetails {
+public class User extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("회원 인덱스")
     private Long id;
 
+    @Comment("회원 코드")
+    private String code;
+
     @Column(name = "user_id", nullable = false)
+    @Comment("아이디")
     private String userId;
 
     @Column(nullable = false)
+    @Comment("패스워드")
     private String password;
 
+    @Comment("이름")
     private String name;
 
     @Enumerated(EnumType.STRING)
-    private Role roles;
+    @ColumnDefault("'NONE'")
+    @Comment("성별")
+    private UserGenderType gender;
 
+    @Comment("체중")
+    private Double weight;
 
-    @Builder
-    public User(String userId, String password, String name, Role roles) {
+    @Comment("키")
+    private Double height;
+
+    @Enumerated(EnumType.STRING)
+    @ColumnDefault("'WAIT'")
+    @Comment("회원 상태")
+    private UserStatusType status;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles = new HashSet<>();
+
+    protected User() {}
+
+    // 회원 가입
+    public User(String code, String userId, String password, String name, UserGenderType gender, Double weight, Double height, UserStatusType status, Set<Role> roles) {
+        this.code = code;
         this.userId = userId;
         this.password = password;
         this.name = name;
+        this.gender = gender;
+        this.weight = weight;
+        this.height = height;
+        this.status = status;
         this.roles = roles;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.roles.name()));
-        return authorities;
-    }
-    @Override
-    public String getUsername() {
-        return userId;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 
 }
