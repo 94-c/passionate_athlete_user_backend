f81ddf7b6e7ceffa81e4b82cab0deb731ecee17d
94-c <hyeongwoo26@gmail.com>
2024-05-24 13:32:28 +0900

    refactor : 주석 제거

--- src/main/java/com/backend/athlete/domain/attendance/service/AttendanceService.java
+++ src/main/java/com/backend/athlete/domain/attendance/service/AttendanceService.java
@@ -4,7 +4,6 @@ import com.backend.athlete.domain.attendance.dto.request.CreateAttendanceEventRe
 import com.backend.athlete.domain.attendance.dto.response.CreateAttendanceEventResponse;
 import com.backend.athlete.domain.attendance.model.Attendance;
 import com.backend.athlete.domain.attendance.repository.AttendanceRepository;
-import com.backend.athlete.domain.physical.repository.PhysicalRepository;
 import com.backend.athlete.domain.user.model.User;
 import com.backend.athlete.domain.user.repository.UserRepository;
 import com.backend.athlete.global.exception.ServiceException;
@@ -31,15 +30,13 @@ public class AttendanceService {
             throw new ServiceException("회원을 찾을 수 없습니다.");
         }
 
-        if (dto.getEventDate() == null) {
-            dto.setEventDate(LocalDate.now());
-        }
+        if (dto.getEventDate() == null) dto.setEventDate(LocalDate.now());
 
         LocalDate eventDate = dto.getEventDate();
+
         if (attendanceRepository.findByUserIdAndAttendanceDate(user.getId(), eventDate).isPresent()) {
             throw new ServiceException("이미 해당 " +  eventDate + " 일에 출석 했습니다. ");
         }
-
 
         Attendance attendance = attendanceRepository.save(CreateAttendanceEventRequest.toEntity(dto, user));
 
