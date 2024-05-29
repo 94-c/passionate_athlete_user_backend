1abc216d28837134ab7de3a0ad29bf149b62342f
94-c <hyeongwoo26@gmail.com>
2024-05-29 20:25:04 +0900

    feat : api별로 권한 부여

--- src/main/java/com/backend/athlete/domain/athlete/controller/AthleteController.java
+++ src/main/java/com/backend/athlete/domain/athlete/controller/AthleteController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +32,7 @@ public class AthleteController {
      * 4. 데일리 운동 기록 삭제
      */
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('USER')")
     public ResponseEntity<CreateAthleteResponseDto> createAthlete(@AuthenticationPrincipal CustomUserDetailsImpl userPrincipal,
                                                                   @Valid @RequestBody CreateAthleteRequestDto dto) {
         CreateAthleteResponseDto createAthlete = athleteService.createAthlete(userPrincipal, dto);
@@ -54,6 +56,7 @@ public class AthleteController {
     }
 
     @DeleteMapping("/daily/{id}")
+    @PreAuthorize("hasAnyAuthority('USER')")
     public ResponseEntity<Void> deleteDailyAthlete(@AuthenticationPrincipal CustomUserDetailsImpl userPrincipal,
                                                    @PathVariable Long id) {
         athleteService.deleteDailyAthleteById(userPrincipal, id);
