c7f4bb06189bb6aaa57c22dc7762a1bcc26f7517
94-c <hyeongwoo26@gmail.com>
2024-06-02 02:42:02 +0900

    refactor : 클래스명 변경

--- src/main/java/com/backend/athlete/domain/notice/dto/response/GetNoticeCommentResponse.java
+++ src/main/java/com/backend/athlete/domain/notice/dto/response/GetNoticeCommentResponse.java
@@ -1,25 +1,25 @@
 package com.backend.athlete.domain.notice.dto.response;
 
-import com.backend.athlete.domain.notice.model.Comment;
+import com.backend.athlete.domain.comment.model.Comment;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class GetCommentResponse {
+public class GetNoticeCommentResponse {
     private Long id;
     private String content;
     private String userName;
     private String createdAt;
 
-    public GetCommentResponse(Long id, String content, String userName, String createdAt) {
+    public GetNoticeCommentResponse(Long id, String content, String userName, String createdAt) {
         this.id = id;
         this.content = content;
         this.userName = userName;
         this.createdAt = createdAt;
     }
-    public static GetCommentResponse fromEntity(Comment comment) {
-        return new GetCommentResponse(
+    public static GetNoticeCommentResponse fromEntity(Comment comment) {
+        return new GetNoticeCommentResponse(
                 comment.getId(),
                 comment.getContent(),
                 comment.getUser().getName(),
