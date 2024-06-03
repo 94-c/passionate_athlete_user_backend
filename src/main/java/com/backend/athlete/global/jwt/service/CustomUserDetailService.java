cd776476a10dc2e1dcad5fc09f62e18ff2d9b8fa
HyeongWooChoi <hw.choi@catenoid.net>
2024-06-03 14:31:05 +0900

    feat : 회원 상태 WAIT 일 떄 홈페이지 접속 차단

--- src/main/java/com/backend/athlete/global/jwt/service/CustomUserDetailService.java
+++ src/main/java/com/backend/athlete/global/jwt/service/CustomUserDetailService.java
@@ -2,7 +2,9 @@ package com.backend.athlete.global.jwt.service;
 
 import com.backend.athlete.domain.auth.repository.AuthRepository;
 import com.backend.athlete.domain.user.model.User;
+import com.backend.athlete.domain.user.model.type.UserStatusType;
 import com.backend.athlete.global.exception.ResourceNotFoundException;
+import com.backend.athlete.global.exception.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -24,6 +26,11 @@ public class CustomUserDetailService implements UserDetailsService {
     public CustomUserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = authRepository.findByUserId(username).orElseThrow(
                 () -> new ResourceNotFoundException("USER", "User ID : " , username));
+
+        if (user.getStatus() == UserStatusType.WAIT) {
+            throw new ServiceException("회원이 활성화되지 않았습니다.");
+        }
+
         return CustomUserDetailsImpl.build(user);
     }
 }
