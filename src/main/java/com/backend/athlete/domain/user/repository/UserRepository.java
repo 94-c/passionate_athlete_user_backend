181c279ae3b05349284bdb0d8308ef7282531638
94-c <hyeongwoo26@gmail.com>
2024-06-03 14:33:33 +0900

    Revert "feat : 지점 관리 개발 완료 (전체보기, 등록, 상세보기, 수정, 지점 회원 검색)"
    This reverts commit 21dc1aed296cfa7f3c795aeb56aab7f9c3701873.

--- src/main/java/com/backend/athlete/domain/user/repository/UserRepository.java
+++ src/main/java/com/backend/athlete/domain/user/repository/UserRepository.java
@@ -1,18 +1,11 @@
 package com.backend.athlete.domain.user.repository;
 
-import com.backend.athlete.domain.branch.model.Branch;
 import com.backend.athlete.domain.user.model.User;
-import com.backend.athlete.domain.user.model.type.UserRoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserId(String userId);
-    @Query("SELECT u FROM User u JOIN u.roles r WHERE u.branch = :branch AND r.name = :role ORDER BY u.createdDate DESC")
-    List<User> findByBranchAndRole(@Param("branch") Branch branch, @Param("role") UserRoleType role);
 
 }
