d5aaa70612d19892d733953317341319087f8b66
94-c <hyeongwoo26@gmail.com>
2024-06-05 14:08:01 +0900

    feat : UserCodeUtils 중복체크

--- src/main/java/com/backend/athlete/support/util/UserCodeUtils.java
+++ src/main/java/com/backend/athlete/support/util/UserCodeUtils.java
@@ -1,11 +1,24 @@
 package com.backend.athlete.support.util;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class UserCodeUtils {
+    private static final Set<String> generatedCodes = new HashSet<>();
+
     public static String generateRandomString() {
+        String code;
         String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-        return "G-" + uuid.substring(0, 6);
+        code = "G-" + uuid.substring(0, 6);
+
+        while (generatedCodes.contains(code)) {
+            uuid = UUID.randomUUID().toString().replaceAll("-", "");
+            code = "G-" + uuid.substring(0, 6);
+        }
+
+        generatedCodes.add(code);
+        return code;
     }
 
 }
