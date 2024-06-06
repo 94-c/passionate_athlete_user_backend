ea8877f9943c8a9133fede9ce583a1f53014a10f
94-c <hyeongwoo26@gmail.com>
2024-06-06 20:30:55 +0900

    refactor : report 리팩토링

--- src/main/java/com/backend/athlete/application/ReportService.java
+++ src/main/java/com/backend/athlete/application/ReportService.java
@@ -9,6 +9,7 @@ import com.backend.athlete.domain.user.User;
 import com.backend.athlete.domain.user.UserRepository;
 import com.backend.athlete.presentation.report.response.GetWeeklyAttendanceResponse;
 import com.backend.athlete.support.jwt.service.CustomUserDetailsImpl;
+import com.backend.athlete.support.util.FindUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,42 +34,25 @@ public class ReportService {
 
     @Transactional
     public GetWeeklyAttendanceResponse getWeeklyAttendance(CustomUserDetailsImpl userPrincipal, LocalDate week) {
-        User findUser = userRepository.findByUserId(userPrincipal.getUsername());
-        LocalDate startOfWeek = week.with(DayOfWeek.MONDAY);
-        LocalDate endOfWeek = week.with(DayOfWeek.SUNDAY);
+        User user = FindUtils.findByUserId(userPrincipal.getUsername());
+        LocalDate startOfWeek = week.with(java.time.DayOfWeek.MONDAY);
+        LocalDate endOfWeek = week.with(java.time.DayOfWeek.SUNDAY);
 
-        List<Attendance> attendances = attendanceRepository.findByUserAndAttendanceDateBetween(findUser, startOfWeek, endOfWeek);
-        List<Athlete> athletes = athleteRepository.findByUserAndDailyTimeBetween(findUser, startOfWeek, endOfWeek);
-
-        List<LocalDate> presentDays = attendances.stream()
-                .map(Attendance::getAttendanceDate)
-                .collect(Collectors.toList());
-
-        List<LocalDate> successDays = athletes.stream()
-                .filter(athlete -> athlete.getType() == AthleteSuccessType.SUCCESS)
-                .map(Athlete::getDailyTime)
-                .collect(Collectors.toList());
-
-        List<LocalDate> failDays = athletes.stream()
-                .filter(athlete -> athlete.getType() == AthleteSuccessType.FAIL)
-                .map(Athlete::getDailyTime)
-                .collect(Collectors.toList());
-
-        int totalDaysPresent = presentDays.size();
-        int totalDaysSuccess = successDays.size();
-        int totalDaysFail = failDays.size();
-
-        return GetWeeklyAttendanceResponse.fromEntity(totalDaysPresent, totalDaysSuccess, totalDaysFail, presentDays, successDays, failDays);
+        return getAttendanceData(user, startOfWeek, endOfWeek);
     }
 
     @Transactional
     public GetWeeklyAttendanceResponse getMonthlyAttendance(CustomUserDetailsImpl userPrincipal, YearMonth month) {
-        User findUser = userRepository.findByUserId(userPrincipal.getUsername());
+        User user = FindUtils.findByUserId(userPrincipal.getUsername());
         LocalDate startDate = month.atDay(1);
         LocalDate endDate = month.atEndOfMonth();
 
-        List<Attendance> attendances = attendanceRepository.findByUserAndAttendanceDateBetween(findUser, startDate, endDate);
-        List<Athlete> athletes = athleteRepository.findByUserAndDailyTimeBetween(findUser, startDate, endDate);
+        return getAttendanceData(user, startDate, endDate);
+    }
+
+    private GetWeeklyAttendanceResponse getAttendanceData(User user, LocalDate startDate, LocalDate endDate) {
+        List<Attendance> attendances = attendanceRepository.findByUserAndAttendanceDateBetween(user, startDate, endDate);
+        List<Athlete> athletes = athleteRepository.findByUserAndDailyTimeBetween(user, startDate, endDate);
 
         List<LocalDate> presentDays = attendances.stream()
                 .map(Attendance::getAttendanceDate)
