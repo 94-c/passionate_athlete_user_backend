794b0d4557effe80d9c825c02076c3f0f8eefca3
94-c <hyeongwoo26@gmail.com>
2024-06-09 23:51:47 +0900

    feat : 오늘의 운동 저장 기능 완료

--- src/main/java/com/backend/athlete/application/WorkoutService.java
+++ src/main/java/com/backend/athlete/application/WorkoutService.java
@@ -1,28 +1,57 @@
 package com.backend.athlete.application;
 
-import com.backend.athlete.domain.execise.Workout;
-import com.backend.athlete.domain.execise.WorkoutRepository;
+import com.backend.athlete.domain.execise.*;
+import com.backend.athlete.presentation.exercise.request.CreateWorkoutInfoRequest;
 import com.backend.athlete.presentation.exercise.request.CreateWorkoutRequest;
 import com.backend.athlete.presentation.exercise.response.CreateWorkoutResponse;
 import com.backend.athlete.support.exception.ServiceException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class WorkoutService {
     private final WorkoutRepository workoutRepository;
-
-    public WorkoutService(WorkoutRepository workoutRepository) {
+    private final ExerciseRepository exerciseRepository;
+    public WorkoutService(WorkoutRepository workoutRepository, ExerciseRepository exerciseRepository) {
         this.workoutRepository = workoutRepository;
+        this.exerciseRepository = exerciseRepository;
     }
 
     public CreateWorkoutResponse createWorkout(CreateWorkoutRequest request) {
         duplicateWorkoutTitle(request.getTitle());
 
-        Workout workout = workoutRepository.save(CreateWorkoutRequest.toEntity(request));
+        Workout workout = CreateWorkoutRequest.toEntity(request);
 
-        return CreateWorkoutResponse.toEntity(workout);
+        if (request.getWorkoutInfos() != null) {
+            List<WorkoutInfo> workoutInfos = request.getWorkoutInfos().stream()
+                    .map(infoRequest -> {
+                        Exercise exercise = exerciseRepository.findById(infoRequest.getExerciseId())
+                                .orElseThrow(() -> new RuntimeException("Exercise not found"));
+                        return CreateWorkoutInfoRequest.toEntity(infoRequest, workout, exercise);
+                    })
+                    .collect(Collectors.toList());
+
+            workoutInfos.forEach(workout::addWorkoutInfo);
+        }
+
+        Workout savedWorkout = workoutRepository.save(workout);
+        return CreateWorkoutResponse.fromEntity(savedWorkout);
+    }
+
+
+    public List<Workout> getAllWorkouts() {
+        return workoutRepository.findAll();
+    }
+
+    public Workout getWorkoutById(Long id) {
+        return workoutRepository.findById(id).orElse(null);
+    }
+
+    public void deleteWorkout(Long id) {
+        workoutRepository.deleteById(id);
     }
 
     protected void duplicateWorkoutTitle(String title) {
