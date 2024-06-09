de09031268fec971afa4dbb167c800accf499956
94-c <hyeongwoo26@gmail.com>
2024-06-09 23:51:24 +0900

    feat : 오늘의 운동 response 클래스 생성

--- src/main/java/com/backend/athlete/presentation/exercise/response/CreateWorkoutResponse.java
+++ src/main/java/com/backend/athlete/presentation/exercise/response/CreateWorkoutResponse.java
@@ -12,19 +12,25 @@ public class CreateWorkoutResponse {
     private Long id;
     private String title;
     private String description;
+    private List<GetWorkoutInfoResponse> workoutInfos;
 
-    public CreateWorkoutResponse(Long id, String title, String description) {
+    public CreateWorkoutResponse(Long id, String title, String description, List<GetWorkoutInfoResponse> workoutInfos) {
         this.id = id;
         this.title = title;
         this.description = description;
+        this.workoutInfos = workoutInfos;
     }
 
-    public static CreateWorkoutResponse toEntity(Workout workout) {
+    public static CreateWorkoutResponse fromEntity(Workout workout) {
+        List<GetWorkoutInfoResponse> workoutInfoResponses = workout.getWorkoutInfos().stream()
+                .map(GetWorkoutInfoResponse::fromEntity)
+                .collect(Collectors.toList());
+
         return new CreateWorkoutResponse(
                 workout.getId(),
                 workout.getTitle(),
-                workout.getDescription()
+                workout.getDescription(),
+                workoutInfoResponses
         );
     }
-
 }
