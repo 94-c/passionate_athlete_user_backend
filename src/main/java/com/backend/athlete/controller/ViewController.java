7594af75e40a088a34c58b1787c06f1303d45db1
HyeongWooChoi <hw.choi@catenoid.net>
2024-06-13 17:40:14 +0900

    feature : 로그인 화면 구현 완료!

--- src/main/java/com/backend/athlete/controller/ViewController.java
+++ src/main/java/com/backend/athlete/controller/ViewController.java
@@ -1,13 +1,19 @@
 package com.backend.athlete.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("pageTitle", "로그인");
         return "login";
     }
 
+    @GetMapping("/main")
+    public String main() {
+        return "main";
+    }
 }
