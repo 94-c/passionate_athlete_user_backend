ebd7e84d340e4442ee647d566c46fe75ade60e05
94-c <hyeongwoo26@gmail.com>
2024-07-11 15:54:40 +0900

    remove : Exercise, Workout 삭제

--- src/main/java/com/backend/athlete/domain/execise/domain/WorkoutInfoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.athlete.domain.execise.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkoutInfoRepository extends JpaRepository<WorkoutInfo, Long> {
-}
--- src/main/java/com/backend/athlete/domain/execise/domain/type/LevelType.java
+++
@@ -1,5 +0,0 @@
-package com.backend.athlete.domain.execise.domain.type;
-
-public enum LevelType {
-    SS, S, A
-}
