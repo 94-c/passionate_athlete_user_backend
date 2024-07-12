42b592f590ec7d316eece42e783dfbc0e2b245b8
94-c <hyeongwoo26@gmail.com>
2024-07-12 13:32:00 +0900

    feat : 게시물 업로드 시 필요한 파일 도메인 생성

--- src/main/java/com/backend/athlete/domain/file/domain/FileRepository.java
+++ src/main/java/com/backend/athlete/domain/file/domain/FileRepository.java
@@ -1,4 +1,4 @@
-package com.backend.athlete.domain.notice.domain;
+package com.backend.athlete.domain.file.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
