af12486afa14bf9a812607f31c7ea2e532729c27
94-c <hyeongwoo26@gmail.com>
2024-07-26 11:50:28 +0900

    feat : 운동 상세 정보 API 완료

--- src/main/java/com/backend/athlete/domain/workout/domain/WorkoutRecordHistoryRepository.java
+++ src/main/java/com/backend/athlete/domain/workout/domain/WorkoutRecordHistoryRepository.java
@@ -2,5 +2,9 @@ package com.backend.athlete.domain.workout.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface WorkoutRecordHistoryRepository extends JpaRepository<WorkoutRecordHistory, Long> {
+    List<WorkoutRecordHistory> findByWorkoutRecordId(Long workoutRecordId);
 }
