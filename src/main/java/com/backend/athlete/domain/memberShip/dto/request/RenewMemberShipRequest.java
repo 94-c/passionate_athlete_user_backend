511775de78a563d48d70d59efad0ec7ad559620f
94-c <hyeongwoo26@gmail.com>
2024-07-31 14:34:18 +0900

    refactor : 회원권 갱신시 에러 발생으로 인한 코드 수정

--- src/main/java/com/backend/athlete/domain/memberShip/dto/request/RenewMemberShipRequest.java
+++ src/main/java/com/backend/athlete/domain/memberShip/dto/request/RenewMemberShipRequest.java
@@ -1,18 +1,22 @@
 package com.backend.athlete.domain.memberShip.dto.request;
 
+import com.backend.athlete.domain.memberShip.domain.MemberShip;
 import com.backend.athlete.domain.memberShip.domain.type.PeriodType;
+import com.backend.athlete.domain.user.domain.User;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter @Setter
 public class RenewMemberShipRequest {
     @NotNull(message = "기간 유형을 선택 해주세요.")
     private PeriodType periodType;
 
-    public RenewMemberShipRequest(PeriodType periodType) {
-        this.periodType = periodType;
+    public static MemberShip toEntity(RenewMemberShipRequest request, User user) {
+        LocalDate startDate = LocalDate.now();
+        LocalDate expiryDate = startDate.plusMonths(request.getPeriodType().getMonths());
+        return new MemberShip(user, startDate, expiryDate, true);
     }
-
-
 }
