92f9cb1c644bce0e3bd976c8430232f98c44415c
94-c <hyeongwoo26@gmail.com>
2024-08-13 16:52:59 +0900

    refactor : 주석 제거

--- src/main/java/com/backend/athlete/domain/admin/controller/AdminController.java
+++ src/main/java/com/backend/athlete/domain/admin/controller/AdminController.java
@@ -24,6 +24,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AdminController {
 
+    /**
+     * TODO : 어드민 프로젝트로 이관 예정
+     */
     private final AdminService adminService;
 
     @PutMapping("/users/{userId}/status")
